b601b57fd5d6678f6aca9c843c3afe18b050a189
musaifathar <shadab.4thdimensiongroup@gmail.com>
2025-05-31 11:25:20 +0000

    convert var

---
+++ level_01/03_convert.js
@@ -0,0 +1,30 @@
+let score ="Musaif"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "Musaif"
+
+let booleanInLogged = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Musaif" => true 
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
