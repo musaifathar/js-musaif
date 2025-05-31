ba0d6d1b0779dad89bb42d56d39e227c976378d6
musaifathar <shadab.4thdimensiongroup@gmail.com>
2025-05-31 08:01:48 +0000

    data type

---
+++ level_02/datatype.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code and newer version
+
+//alert( 3 + 3 ) // we are using nodejs not browser
+
+console.log( 3
+    +
+    3) //code readability should be hight
+
+    console.log("Musaif")
+
+    let name = "Musaif"
+    let age = 22
+    let isLoggedIn = false
+    let state;
+
+    // number => 2 to 53
+    // Bigint  
+    // string => ""
+    // boolean => true/false
+    // null => standalone value
+    // undefined => undefined
+    // symbol => unique
+
+
+    // object
+
+
+    console.log(typeof undefined); //undefined
+    console.log(typeof null); // object
